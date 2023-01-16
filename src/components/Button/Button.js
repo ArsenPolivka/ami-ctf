a841878a820360433d0bf9a1f6f2e70377f1c374
nmelnychenko <mykolam@seekingalpha.com>
2023-01-16 12:39:58 +0200

    [7]: finish styling desktop version of Header

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,17 +2,17 @@ import { Link } from 'react-router-dom';
 
 import './Button.css';
 
-export const Button = ({ to, children, type }) => {
+export const Button = ({ to, children, type, rootClassName }) => {
   if (to) {
     return (
-      <Link to={to} className={`button button--${type}`}>
+      <Link to={to} className={`button button--${type} ${rootClassName}`}>
         {children}
       </Link>
     );
   }
 
   return (
-    <button className={`button button--${type}`}>
+    <button className={`button button--${type} ${rootClassName}`}>
       {children}
     </button>
   );
--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,40 +1,37 @@
-import { Link } from 'react-router-dom';
+import { PageNavigation } from './PageNavigation';
 
 import { LogoExtended } from '../../components/Logo';
 import { Button } from '../../components/Button';
+import { Container } from '../../components/Layout'
 
 import './Header.css';
 
 export const Header = ({ pageNavigation, hasLogin, hasRegistration}) => {
   return (
     <header className="header">
-      <LogoExtended />
+      <Container>
+        <div className="wrapper">
+          <LogoExtended />
 
-      <nav className="nav">
-        {pageNavigation ? (
-          <ul className="page-navigation">
-            {pageNavigation.map(({ to, label }) => {
-              return (
-                <Link to={to}>
-                  {label}
-                </Link>
-              );
-            })}
-          </ul>
-        ) : null}
+          <nav className="nav">
+            {pageNavigation ? (
+              <PageNavigation navList={pageNavigation} rootClassName="navigation" />
+            ) : null}
 
-        {hasLogin ? (
-          <Button to="/login" type="secondary">
-            Login
-          </Button>
-        ) : null}
+            {hasLogin ? (
+              <Button to="/login" type="secondary" rootClassName="first-button">
+                Login
+              </Button>
+            ) : null}
 
-        {hasRegistration ? (
-          <Button to="/registration" type="primary">
-            Sign Up
-          </Button>
-        ) : null}
-      </nav>
+            {hasRegistration ? (
+              <Button to="/registration" type="primary">
+                Sign Up
+              </Button>
+            ) : null}
+          </nav>
+        </div>
+      </Container>
     </header>
   );
 };
