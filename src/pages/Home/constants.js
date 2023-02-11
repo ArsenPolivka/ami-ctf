1aab24c5a661beaf9438e1b7349d81dc603c2340
nmelnychenko <mykolam@seekingalpha.com>
2023-02-11 19:31:53 +0200

    [16]: finish roadMap markup

--- src/pages/Home/constants.js
+++ src/pages/Home/constants.js
@@ -1,3 +1,8 @@
+import registerImg from './assets/register.png';
+import taskImg from './assets/taskList.png';
+import funImg from './assets/fun.png';
+import danceImg from './assets/dance.png';
+
 export const pageNavigation = [
   {
     to: 'about',
@@ -13,5 +18,32 @@ export const pageNavigation = [
     to: 'contact-us',
     label: 'Contact Us',
     id: 'contact-us',
+  },
+];
+
+export const roadMap = [
+  {
+    id: 'register',
+    label: 'Register',
+    imgSrc: registerImg,
+    alt: 'Successfull registration',
+  },
+  {
+    id: 'task',
+    label: 'Complete the tasks',
+    imgSrc: taskImg,
+    alt: 'Tasks list',
+  },
+  {
+    id: 'fun',
+    label: 'Have fun',
+    imgSrc: funImg,
+    alt: 'two persons have fun',
+  },
+  {
+    id: 'win',
+    label: 'Win prizes',
+    imgSrc: danceImg,
+    alt: 'two dancing persons',
   },
 ];
