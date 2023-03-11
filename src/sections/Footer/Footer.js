61293b1fcb05067088a3cdefd853405f78c4593b
unknown <polivka2003@gmail.com>
2023-03-11 19:59:08 +0200

    #50: footer responsive added, bugs fixed, copyright block upgraded

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -10,21 +10,46 @@ export const Footer = () => {
     return (
         <footer className={styles['footer-section']}>
             <Container>
-                <div className={styles['footer-wrapper']}>
-                    <div className={styles['copyright']}>
-                        <span className={styles['year']}>2023</span>
-                        <span className={styles['copyright-symbol']}>&copy;</span>
-                        <span className={styles['rights']}>All rights reserved</span>
+                    <div className={styles['mobile-variant']}>
+                        <div className={styles['copyright-logo-wrapper']}>
+                            <LogoExtended className={styles['logo-mobile']} theme="white" size="mobile" />
+                            <div className={styles['copyright']}>
+                                <span className={styles['year']}>
+                                    {(new Date().getFullYear())}
+                                </span>
+                                <span className={styles['copyright-symbol']}>&copy;</span>
+                                <span className={styles['rights']}>All rights reserved</span>
+                            </div>
+                        </div>
+                        <div className={styles['social']}>
+                            <a className={styles['telegram-wrapper']} href="https://web.telegram.org/">
+                                <Telegram className={styles['telegram']}/>
+                            </a>
+                            <a className={styles['instagram-wrapper']} href="https://www.instagram.com/">
+                                <Instagram className={styles['telegram']}/>
+                            </a>
+                        </div>
                     </div>
-                    <div className={styles['logo-wrapper']}>
+                    <div className={styles['desktop-variant']}>
+                        <div className={styles['copyright']}>
+                            <span className={styles['year']}>
+                                    {(new Date().getFullYear())}
+                            </span>
+                            <span className={styles['copyright-symbol']}>&copy;</span>
+                            <span className={styles['rights']}>All rights reserved</span>
+                        </div>
+
                         <LogoExtended className={styles['logo-desktop']} theme="white" size="desktop" />
-                        <LogoExtended className={styles['logo-mobile']} theme="white" size="mobile" />
+
+                        <div className={styles['social']}>
+                            <a className={styles['telegram-wrapper']} href="https://web.telegram.org/">
+                                <Telegram className={styles['telegram']}/>
+                            </a>
+                            <a className={styles['instagram-wrapper']} href="https://www.instagram.com/">
+                                <Instagram className={styles['instagram']}/>
+                            </a>
+                        </div>
                     </div>
-                    <div className={styles['social']}>
-                        <Telegram className={styles['telegram']} />
-                        <Instagram className={styles['instagram']} />
-                    </div>
-                </div>
             </Container>
         </footer>
     );
