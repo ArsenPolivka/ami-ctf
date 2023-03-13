f987a1a55b7b4bb7a34f728322e83d274f4ec26d
unknown <polivka2003@gmail.com>
2023-03-13 11:09:52 +0200

    #50: code refactored, bugs fixed

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -12,41 +12,55 @@ export const Footer = () => {
             <Container>
                     <div className={styles['mobile-variant']}>
                         <div className={styles['copyright-logo-wrapper']}>
-                            <LogoExtended className={styles['logo-mobile']} theme="white" size="mobile" />
-                            <div className={styles['copyright']}>
-                                <span className={styles['year']}>
-                                    {(new Date().getFullYear())}
-                                </span>
-                                <span className={styles['copyright-symbol']}>&copy;</span>
-                                <span className={styles['rights']}>All rights reserved</span>
+                            <LogoExtended
+                                className={styles['logo-mobile']}
+                                theme="white"
+                                size="mobile"
+                            />
+                            <div className={styles.copyright}>
+                                <span>2023</span>
+                                <span>&copy;</span>
+                                <span>All rights reserved</span>
                             </div>
                         </div>
-                        <div className={styles['social']}>
-                            <a className={styles['telegram-wrapper']} href="https://web.telegram.org/">
-                                <Telegram className={styles['telegram']}/>
+                        <div className={styles.social}>
+                            <a
+                                className={styles['telegram-wrapper']}
+                                href="https://web.telegram.org/"
+                            >
+                                <Telegram className={styles.telegram} />
                             </a>
-                            <a className={styles['instagram-wrapper']} href="https://www.instagram.com/">
-                                <Instagram className={styles['telegram']}/>
+                            <a
+                                className={styles['instagram-wrapper']}
+                                href="https://www.instagram.com/"
+                            >
+                                <Instagram className={styles.instagram} />
                             </a>
                         </div>
                     </div>
                     <div className={styles['desktop-variant']}>
-                        <div className={styles['copyright']}>
-                            <span className={styles['year']}>
-                                    {(new Date().getFullYear())}
-                            </span>
-                            <span className={styles['copyright-symbol']}>&copy;</span>
-                            <span className={styles['rights']}>All rights reserved</span>
+                        <div className={styles.copyright}>
+                            <span>2023</span>
+                            <span>&copy;</span>
+                            <span>All rights reserved</span>
                         </div>
-
-                        <LogoExtended className={styles['logo-desktop']} theme="white" size="desktop" />
-
-                        <div className={styles['social']}>
-                            <a className={styles['telegram-wrapper']} href="https://web.telegram.org/">
-                                <Telegram className={styles['telegram']}/>
+                        <LogoExtended
+                            className={styles['logo-desktop']}
+                            theme="white"
+                            size="desktop"
+                        />
+                        <div className={styles.social}>
+                            <a
+                                className={styles['telegram-wrapper']}
+                                href="https://web.telegram.org/"
+                            >
+                                <Telegram className={styles.telegram}/>
                             </a>
-                            <a className={styles['instagram-wrapper']} href="https://www.instagram.com/">
-                                <Instagram className={styles['instagram']}/>
+                            <a
+                                className={styles['instagram-wrapper']}
+                                href="https://www.instagram.com/"
+                            >
+                                <Instagram className={styles.instagram}/>
                             </a>
                         </div>
                     </div>
