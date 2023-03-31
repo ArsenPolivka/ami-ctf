8a68595e0b64391372c50fa5fb8f6bcdd8c63514
nmelnychenko <mykolam@seekingalpha.com>
2023-04-01 01:04:27 +0300

    [93] add hook and Context for Sidebar

--- src/sections/TaskSidebar/TaskSidebar.js
+++ src/sections/TaskSidebar/TaskSidebar.js
@@ -1,11 +1,15 @@
+import { useSidebarConfig } from '../../hooks/useSidebarConfig';
+
 import styles from './TaskSidebar.module.css';
 
-export const TaskSidebar = ({ rating }) => {
+export const TaskSidebar = () => {
+  const { showRatingCard } = useSidebarConfig();
+
   return (
     <aside className={styles.aside}>
       <div className={styles.points}>Your points</div>
 
-      {rating ? (
+      {showRatingCard ? (
         <div className={styles.ratings}>Rating</div>
       ) : null}
     </aside>
