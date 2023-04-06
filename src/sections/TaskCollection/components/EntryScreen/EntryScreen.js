00ac6897a4f6602c0e2d002ebbacc80198294122
unknown <polivka2003@gmail.com>
2023-04-06 21:08:51 +0300

    #84: code refactored

--- src/sections/TaskCollection/components/EntryScreen/EntryScreen.js
+++ src/sections/TaskCollection/components/EntryScreen/EntryScreen.js
@@ -13,26 +13,26 @@ export const EntryScreen = ({ isLocked }) => {
 
 	return (
 			isStarted ? (
-					<section className={styles.entry}>
-						<div className={styles.content}>
-							<Content />
-						</div>
-						<div className={styles['button-wrapper']}>
-							<Button variant='primary-purple'
-							        rootClassName={classNames(styles.button, isLocked ? styles.locked : null)}
-							        disabled={isLocked}
-							        onClick={ toggleStart }
-							>
-								Start the quiz
-							</Button>
-							{ isLocked ? (
-									<p className={styles['time-left']}>
-										Quiz will be started in
-										<span className={styles.time}> 9 days 15 hours</span>
-									</p>
-							) : null }
-						</div>
-					</section>
-				) : null
+				<section className={styles.entry}>
+					<div className={styles.content}>
+						<Content />
+					</div>
+					<div className={styles['button-wrapper']}>
+						<Button variant='primary-purple'
+						        rootClassName={classNames(styles.button, isLocked ? styles.locked : null)}
+						        disabled={isLocked}
+						        onClick={ toggleStart }
+						>
+							Start the quiz
+						</Button>
+						{ isLocked ? (
+								<p className={styles['time-left']}>
+									Quiz will be started in
+									<span className={styles.time}> 9 days 15 hours</span>
+								</p>
+						) : null }
+					</div>
+				</section>
+			) : null
 	)
 }
