c323365900df5f442d71a7ddfd55e41c3c537944
nmelnychenko <mykolam@seekingalpha.com>
2023-04-28 23:11:17 +0300

    refactor

--- src/context/event/EventProvider.js
+++ src/context/event/EventProvider.js
@@ -5,7 +5,6 @@ import { getEventStatus } from '../../api/event';
 
 export const EventProvider = ({ children }) => {
 	const [eventStatus, setEventStatus] = useState(false);
-	const [eventLockedStatus, setEventLockedStatus] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
@@ -14,7 +13,6 @@ export const EventProvider = ({ children }) => {
 
 			if (eventStatusResponse.id) {
 				setEventStatus(eventStatusResponse.status);
-				setEventLockedStatus(eventStatusResponse);
 				setIsLoading(false);
 			} else {
 				setIsLoading(false);
@@ -29,7 +27,7 @@ export const EventProvider = ({ children }) => {
 	}
 
 	return (
-			<EventContext.Provider value={{ eventStatus, setEventStatus, eventLockedStatus }}>
+			<EventContext.Provider value={{ eventStatus, setEventStatus }}>
 				{children}
 			</EventContext.Provider>
 	);
