b60e30ffc3c678b7536285d942182ad5f4499c68
Arsen Polivka <52714918+ArsenPolivka@users.noreply.github.com>
2023-05-02 13:33:26 +0300

    #156: sign-up/sign-in pages fixed (#157)
    * #156: sign-up/sign-in pages fixed
    
    * #156: code refactored
    
    * #156: loader updated

--- src/sections/LoginForm/LoginForm.js
+++ src/sections/LoginForm/LoginForm.js
@@ -9,6 +9,7 @@ import { Button } from '../../components/Button';
 import { loginUser } from '../../api/user';
 import { AuthContext } from '../../context/auth/context';
 import { Loader } from "../../components/Loader";
+import { ReactComponent as Illustration } from "./assets/login-icon.svg";
 
 import styles from './LoginForm.module.css';
 
@@ -60,55 +61,64 @@ export const LoginForm = () => {
   return (
     <div className={styles.wrapper}>
       <Container>
-        { isLoading ? <Loader /> : (
-            <form className={styles.form} onSubmit={handleSubmit}>
-              <h2 className={styles.title}>Sign in</h2>
+        <div className={styles['form-wrapper']}>
+          <form
+              className={styles.form}
+              onSubmit={ handleSubmit }
+          >
+            <h2 className={styles.title}>Sign in</h2>
 
-              <div className={styles.row}>
-                <Input
-                    hideLabel
-                    required
-                    inputRootClassName={styles.input}
-                    name="email"
-                    label="E-mail"
-                    placeholder="E-mail"
-                    error={error && error.email}
-                    onChange={handleChange}
-                />
-              </div>
+            <div className={styles.row}>
+              <Input
+                  hideLabel
+                  required
+                  inputRootClassName={styles.input}
+                  name="email"
+                  label="E-mail"
+                  placeholder="E-mail"
+                  error={error && error.email}
+                  onChange={handleChange}
+              />
+            </div>
 
-              <div className={styles.row}>
-                <Input
-                    hideLabel
-                    required
-                    inputRootClassName={styles.input}
-                    name="password"
-                    type="password"
-                    label="Password"
-                    placeholder="Password"
-                    error={error && error.password}
-                    onChange={handleChange}
-                />
+            <div className={styles.row}>
+              <Input
+                  hideLabel
+                  required
+                  inputRootClassName={styles.input}
+                  name="password"
+                  type="password"
+                  label="Password"
+                  placeholder="Password"
+                  error={error && error.password}
+                  onChange={handleChange}
+              />
 
-                <Link
-                    to="/registration"
-                    className={styles.link}
-                >
-                  Doesn't have an accout yet?
-                </Link>
-              </div>
+              <Link
+                  to="/registration"
+                  className={styles.link}
+              >
+                Doesn't have an accout yet?
+              </Link>
+            </div>
 
-              <div className={classNames(styles.row, styles['last-row'])}>
-                <Button
-                    wide
-                    variant="primary"
-                    type="submit"
-                >
-                  Sign in
-                </Button>
-              </div>
-            </form>
-            )}
+            <div className={classNames(styles.row, styles['last-row'])}>
+              <Button
+                  wide
+                  variant="primary"
+                  type="submit"
+              >
+                Sign in
+              </Button>
+            </div>
+
+            { isLoading ? <Loader /> : null }
+          </form>
+
+          <div className={styles.illustration}>
+            <Illustration />
+          </div>
+        </div>
       </Container>
     </div>
   );
